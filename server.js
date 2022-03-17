77887a621503608b183166e44bdb1e01f4967d5d
Ignacio SuarezB <suarezb.ignacio@gmail.com>
2022-03-17 16:31:33 -0300

    desafio motores de plantillas

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
+const axios = require("axios").default;
 const { errorResponder, invalidPathHandler } = require("./error");
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/products", async (req, res, next) => {
+  try {
+    const allProducts = await axios
+      .get("http://localhost:3000/api/products")
+      .then((res) => res.data);
+
+    res.render("productsPage.ejs", { allProducts });
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.use("/api/products", require("./productsApiRouter"));
 
